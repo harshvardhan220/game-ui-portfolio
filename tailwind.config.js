0537e88ff291e6485fc9bc9fc541bb72526db137
Harshvardhan Shete <harshvardhangithub@gmail.com>
2024-02-06 22:37:50 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        xxs:'375px',
+        xs: "480px", // Extra small devices
+        sm: "640px", // Small devices, like mobile (default)
+        md: "768px", // Medium devices, like tablets
+        lg: "1024px", // Large devices, like laptops
+        xl: "1280px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        "big-shoulders": ["Big Shoulders Display"],
       },
     },
   },
